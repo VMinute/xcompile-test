3ad181d5dee573ca2b99fd2cca7784fa69cdb665
Valter Minute <valter.minute@toradex.com>
2019-11-11 20:42:17 +0100

    run build as user
    Signed-off-by: Valter Minute <valter.minute@toradex.com>

--- runtest.sh
+++ runtest.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
+echo "building containers..."
+docker build -t openttd-native-x64 -f Dockerfile.x64 .
+docker build -t openttd-cross-arm32 -f Dockerfile.cross .
+docker build -t openttd-native-arm32 -f Dockerfile.arm32 .
+
+exit
+
 echo "cloning source repo..."
 cd ..
 rm -fr OpenTTD
 git clone https://github.com/OpenTTD/OpenTTD.git
 cd -
-echo "building containers..."
-docker build -t openttd-native-x64 -f Dockerfile.x64 .
-docker build -t openttd-cross-arm32 -f Dockerfile.cross .
-docker build -t openttd-native-arm32 -f Dockerfile.arm32 .
 
 echo "TEST 1 - local native build"
 SECONDS=0
