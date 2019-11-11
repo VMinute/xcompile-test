c495f129c9532a1bdc7b57ff3b097b255e4e6d41
Ubuntu <valter@openTTD.0fkj5p1qaieebm5gmhcimhggsa.ax.internal.cloudapp.net>
2019-11-11 18:57:41 +0000

    fix setup script
    Signed-off-by: Ubuntu <valter@openTTD.0fkj5p1qaieebm5gmhcimhggsa.ax.internal.cloudapp.net>

--- setup.sh
+++ setup.sh
@@ -1,17 +1,20 @@
 #!/bin/sh
 
 # install docker
+echo "Installing docker..."
 apt install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt update
 apt install docker-ce
-usermod -aG docker ${USER}
+usermod -aG docker $1
 
 # enable arm32 emulation in docker
+echo "Enabling qemu for docker containers..."
 docker run --rm --privileged docker/binfmt:66f9012c56a8316f9244ffd7622d7c21c1f6f28d
 
 # add libraries required for local build
+echo "Installing libraries for local build..."
 apt-get update && apt-get install -y \
     git \
     build-essential \
